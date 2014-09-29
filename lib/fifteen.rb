b5f6517a0e99125e4f73f56268ecd97dfc1a851e
Jose <jrchg1403@gmail.com>
2014-09-28 23:10:48 -0400

    Having problems with 'Stack level too deep'

--- lib/fifteen.rb
+++ lib/fifteen.rb
@@ -1,63 +1,75 @@
 require 'pry'
-# class Fifteen
-class Fifteen
-  def play_game name  #def initialize
-    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    player_number = []
-    computer_number = []
-    #end
-    player_total = calculate_total (player_number)
-    computer_total = calculate_total (computer_number)
 
-    if player_total == 15
-      display_numbers player_number, computer_number
-      puts "Congratulations! You Won!"
-      return 1
-    elsif computer_number == 15
-      display_numbers player_number, computer_number
-      puts "I'm sorry, #{player_name}! I win :D "
-      return -1
+class Game
+  def initialize
+    @numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @player_choose = []
+    @computer_choose = []
+  end
+
+  def sum(array)
+    array.inject{ |sum, x| sum + x}
+  end
+
+  def turn
+    puts "hi"
+    @turnboth = [0, 1]
+    result = @turnboth.sample
+    if result == 1
+      player_turn
+    else
+      computer_turn
     end
+  end
 
-
-
+  def player_turn
     puts "Choose a number between 1 and 9"
-    player_number1 = gets.chomp.to_i
-
-
-
-    until [1,2,3,4,5,6,7,8,9].include?(player_number) do
+    @player_choose = gets.chomp.to_i
+    if numbers.include?(@player_choose) == false
       puts "Hey. You only can choose a number between 1 and 9. No less no more"
-      player_number1 = gets.chomp.to_i
-      computer_rand1 = rand(1..9)
-      puts "Hey. You only can choose a number between 1 and 9. No less no more"
-      player_number2 = gets.chomp.to_i
-      until player_number1 == player_number2
-        puts "I'm sorry, you already choose that number. Pick another that cannot be #{player_number1}"
-        player_number2 =  gets.chomp.to_i
-        computer_rand2 = rand(1..9)
-        until computer_rand1 == computer_rand2
-          computer_rand(1..9)
-        end
-      end
+      player_turn
     end
+    @numbers.delete(@player_choose)
+  end
+
+  def score_pla
+    player_numbers = @player_choose.combination(3).to_a
+    if player_numbers.any? {|array| sum(array) == 15}
+      puts "You Win! Lucky bastard"
+      exit
+    else
+      computer_turn
+    end
+  end
+
+  def computer_turn
+    @computer_choose = @numbers.sample
+    puts " I pick the number #{@computer_choose}"
+    @numbers.delete(@computer_choose)
+    computer_turn
+  end
+
+  def score_computer
+    computer_numbers = @player_choose.combination(3).to_a
+    if computer_numbers.any? {|array| sum(array) == 15}
+      puts "HAHA I win"
+    else
+      player_turn
+    end
+  end
+
+  def start
+    game1 = Game.new
+    puts "Hi there. We are going to play 'Fifteen'. But first tell me your name: "
+    name = gets.chomp
+    puts "Great #{name}! lets play!!"
+    game1.turn
+    game1.player_turn
+    game1.computer_turn
   end
 end
 
-def display_numbers player_number, computer_number
-  puts "My cards: #{computer_number}"
-  puts "My total: #{calculate_total(computer_number)}"
-  puts "Your cards: #{player_number}"
-  puts "Your total: #{calculate_total(player_number)}"
-end
-
-def player_name
-  puts "Hi there. We are going to play 'Fifteen'. But first tell me your name: "
-  player_name = gets.chomp
-  puts "Great #{player_name}! lets play!!"
-end
 
 
-fifteen = Fifteen.new
-fifteen.start
-# end
+game1 = Game.new
+game1.start
